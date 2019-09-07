5975ec81566c06e641bd571749aa7bdc41fe068e
rtspw <richard.cx.tang@gmail.com>
2019-09-07 15:44:39 -0700

    Working k-map solving algorithm

--- index.js
+++ index.js
@@ -41,8 +41,13 @@ function generateMasks(numOfTerms, fixedIndicies = []) {
 
 class Minterm {
   constructor(minterm = '') {
-    this.terms = convertBinaryStrToBoolArr(minterm);
-    this.isClaimed = false;
+    if (typeof minterm === 'string') {
+      this.terms = convertBinaryStrToBoolArr(minterm);
+    } else if (typeof minterm === 'number') {
+      this.terms = convertBinaryStrToBoolArr(convertToBinaryString(minterm));
+    } else {
+      this.terms = minterm;
+    }
   }
 
   getTerm(index) {
@@ -50,6 +55,14 @@ class Minterm {
       return null;
     }
     return this.terms[index];
+  }
+
+  getBinaryString() {
+    return this.terms.map(term => term ? '1' : '0').join('');
+  }
+
+  getDecimal() {
+    return parseInt(this.getBinaryString(), 2);
   }
 
   generateNeighborTerms(fixedIndicies = []) {
@@ -60,41 +73,98 @@ class Minterm {
     if (numOfFreeTerms < 0) return [];
 
     const permutationMasks = generateMasks(numOfFreeTerms, filteredFixedIndicies);
-    const neighbors = permutationMasks.map(mask => xorBoolArrays(mask, this.terms));
+    const neighbors = permutationMasks
+      .map(mask => xorBoolArrays(mask, this.terms))
+      .map(neighbor => new Minterm(neighbor));
+
     return neighbors;
+  }
+
+  equals(other) {
+    if (this.terms.length !== other.terms.length) return false;
+    for (let i = 0; i < this.terms.length; i++) {
+      if (this.terms[i] !== other.terms[i]) return false;
+    }
+    return true;
   }
 }
 
 
 class MintermList {
-  constructor(numOfVariables = 1, baseTenMinterms = [0]) {
+  constructor(numOfVariables = 1, baseTenMinterms = []) {
+    this.numOfVariables = numOfVariables;
     let mintermStrings = baseTenMinterms
       .map(term => convertToBinaryString(term, numOfVariables))
       .filter(term => term.length <= numOfVariables);
     this.minterms = mintermStrings.map(term => new Minterm(term));
   }
 
+  containsMinterm(targetMinterm) {
+    for (const minterm of this.minterms) {
+      if (minterm.equals(targetMinterm)) return true;
+    }
+    return false;
+  }
+
+  containsMinterms(targetMinterms) {
+    for (const targetMinterm of targetMinterms) {
+      if (!this.containsMinterm(targetMinterm)) return false;
+    }
+    return true;
+  }
+
+  addMinterm(minterm) {
+    this.minterms.push(minterm);
+  }
+
   generateGroups() {
     const mintermQueue = [...this.minterms];
+    const groups = [];
+
+    const fixedIndiciesList = generateFixedIndicies(this.numOfVariables);
+    const visitedMinterms = new MintermList(this.numOfVariables);
+
     while (mintermQueue.length > 0) {
       const front = mintermQueue.shift();
-      if (front.isClaimed) continue;
+      console.log('front:', front);
+      console.dir(visitedMinterms, {depth: 100});
+      if (visitedMinterms.containsMinterm(front)) continue;
+      console.log('passed qc');
 
-      const neighbors = front.generateNeighborTerms();
+
+      for (const fixedIndicies of fixedIndiciesList) {
+        const neighbors = front.generateNeighborTerms(fixedIndicies);
+        if (!this.containsMinterms(neighbors)) continue;
+
+        visitedMinterms.addMinterm(front);
+        neighbors.forEach(neighbor => visitedMinterms.addMinterm(neighbor));
+        groups.push([front,...neighbors]);
+        break;
+      }
     }
+
+    return groups;
   }
 }
 
 function generateFixedIndicies(numOfTerms) {
-  const fixedIndiciesList = generateMasks(numOfTerms).map(mask => {
-    const indicies = range(numOfTerms);
-    mask.forEach((bool, idx) => {
-      if (!bool) indicies[idx] = -1;
+  const fixedIndiciesList = generateMasks(numOfTerms)
+    .map(mask => {
+      const indicies = range(numOfTerms);
+      mask.forEach((bool, idx) => {
+        if (!bool) indicies[idx] = -1;
+      });
+      return indicies;
     });
-    return indicies;
-  });
-  return fixedIndiciesList;
+  const allFixed = [];
+  return [allFixed, ...fixedIndiciesList];
 }
 
-console.dir(new MintermList(3, [1, 2, 3, 4]), {depth: 100})
-generateFixedIndicies(3);
+const test = new MintermList(3, [0, 1, 3, 6]).generateGroups();
+console.log(' ');
+console.log(' ');
+console.log(' ');
+console.dir(test, {depth: 100});
+
+console.log(new Minterm(3).getBinaryString());
+console.log(new Minterm(3).getDecimal());
